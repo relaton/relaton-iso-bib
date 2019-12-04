b9198e486b798a67a9abfd3d02eae4826f707d59
Andrei Kislichenko <android.2net@gmail.com>
2019-12-04 16:51:51 +0100

    add grammar_hash function relaton/relaton#54

--- lib/relaton_iso_bib.rb
+++ lib/relaton_iso_bib.rb
@@ -1,7 +1,15 @@
 require "relaton_iso_bib/version"
 require "relaton_iso_bib/iso_bibliographic_item"
+require "digest/md5"
 
 module RelatonIsoBib
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Returns hash of XML reammar
+  def self.grammar_hash
+    gem_path = File.expand_path "..", __dir__
+    grammars_path = File.join gem_path, "grammars", "*"
+    grammars = Dir[grammars_path].map { |gp| File.read gp }.join
+    Digest::MD5.hexdigest grammars
+  end
 end
--- spec/relaton_iso_bib_spec.rb
+++ spec/relaton_iso_bib_spec.rb
@@ -2,4 +2,10 @@ RSpec.describe RelatonIsoBib do
   it "has a version number" do
     expect(RelatonIsoBib::VERSION).not_to be nil
   end
+
+  it "returs grammar hash" do
+    hash = RelatonIsoBib.grammar_hash
+    expect(hash).to be_instance_of String
+    expect(hash.size).to eq 32
+  end
 end
