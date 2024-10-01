2e638b2eb00f30d9ca1f94a3a8eda6fe8a5bbbaf
Andrei Kislichenko <android.2net@gmail.com>
2024-10-01 15:33:56 -0400

    fixes relaton/relaton-iso#171

--- lib/relaton_iso_bib/document_type.rb
+++ lib/relaton_iso_bib/document_type.rb
@@ -3,7 +3,7 @@ module RelatonIsoBib
     DOCTYPES = %w[
       international-standard technical-specification technical-report
       publicly-available-specification international-workshop-agreement guide
-      recommendation amendment technical-corrigendum directive committee-document
+      recommendation amendment technical-corrigendum directive committee-document addendum
     ].freeze
 
     #
--- lib/relaton_iso_bib/version.rb
+++ lib/relaton_iso_bib/version.rb
@@ -1,3 +1,3 @@
 module RelatonIsoBib
-  VERSION = "1.19.0".freeze
+  VERSION = "1.19.1".freeze
 end
